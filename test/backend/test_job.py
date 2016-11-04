4775dfc300015f44a9495f5aaa472961bf8737b2
Ben Webb <ben@salilab.org>
2016-11-04 18:48:04 +0000

    Add basic testcases.

---
+++ test/backend/test_job.py
@@ -0,0 +1,26 @@
+import unittest
+import foxsdock
+import saliweb.test
+import saliweb.backend
+import os
+
+class JobTests(saliweb.test.TestCase):
+    """Check custom FoXSDock Job class"""
+
+    def test_run_ok(self):
+        """Test successful run method"""
+        j = self.make_test_job(foxsdock.Job, 'RUNNING')
+        d = saliweb.test.RunInDir(j.directory)
+        open('input.txt', 'w').write('input line1\ninput line2\n')
+        cls = j.run()
+        self.assert_(isinstance(cls, saliweb.backend.SGERunner),
+                     "SGERunner not returned")
+
+    def test_complete(self):
+        """Test complete() method"""
+        j = self.make_test_job(foxsdock.Job, 'RUNNING')
+        d = saliweb.test.RunInDir(j.directory)
+        j.complete()
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/frontend/frontend.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+
+use saliweb::Test;
+use Test::More 'no_plan';
+
+BEGIN {
+    use_ok('foxsdock');
+}
+
+my $t = new saliweb::Test('foxsdock');
+
+# Test get_navigation_links
+{
+    my $self = $t->make_frontend();
+    my $links = $self->get_navigation_links();
+    isa_ok($links, 'ARRAY', 'navigation links');
+    like($links->[0], qr#<a href="http://modbase/top/">FoXSDock Home</a>#,
+         'Index link');
+    like($links->[1],
+         qr#<a href="http://modbase/top/queue.cgi">FoXSDock Current queue</a>#,
+         'Queue link');
+}
