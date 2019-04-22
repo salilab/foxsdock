ae4d0a310d7e546343b7b9cda2ed77d0cb800af0
Ben Webb <ben@salilab.org>
2019-04-22 13:04:25 -0700

    Test utility functions

--- test/frontend/frontend.pl
+++ test/frontend/frontend.pl
@@ -2,6 +2,8 @@
 
 use saliweb::Test;
 use Test::More 'no_plan';
+use Test::Exception;
+use File::Temp qw(tempdir);
 
 BEGIN {
     use_ok('foxsdock');
@@ -62,3 +64,38 @@ my $t = new saliweb::Test('foxsdock');
     like($txt, qr/Weighted SAXS scoring/ms,
          'get_index_page');
 }
+
+# Test removeSpecialChars
+{
+    is(foxsdock::removeSpecialChars("abcd 1234 %!_-. ABCD"), "abcd1234_.ABCD",
+       "removeSpecialChars");
+}
+
+# Test trimExtension
+{
+    is(foxsdock::trimExtension("abcd"), "abcd",
+       "trimExtension (no extension)");
+    is(foxsdock::trimExtension("1234.txt"), "1234",
+       "trimExtension (3-letter extension)");
+    is(foxsdock::trimExtension("5678.a"), "5678.a",
+       "trimExtension (1-letter extension)");
+}
+
+# Test get_profile_filename
+{
+    my $tmpdir = tempdir(CLEANUP=>1);
+    ok(chdir($tmpdir), "chdir into tempdir");
+
+    dies_ok { foxsdock::get_profile_filename() }
+            "get_profile_filename, no input.txt";
+
+    ok(open(FH, ">input.txt"));
+    print FH "test.pdb ICAM.pdb --saxs 6.dat --complex_type Default";
+    ok(close(FH));
+
+    is(foxsdock::get_profile_filename(), "6.dat", "get_profile_filename, ok");
+
+    chdir("/");
+}
+
+
