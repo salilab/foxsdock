c917bc13bb698268a9b77f7391b9a489763f9228
Ben Webb <ben@salilab.org>
2021-09-27 15:00:43 -0700

    Add more tests of submit page

--- test/frontend/test_submit.py
+++ test/frontend/test_submit.py
@@ -80,6 +80,134 @@ class Tests(saliweb.test.TestCase):
                            re.MULTILINE | re.DOTALL)
             self.assertRegex(rv.data, r)
 
+    def test_submit_weighted(self):
+        """Test submit with weighted SAXS score option"""
+        with tempfile.TemporaryDirectory() as tmpdir:
+            incoming = os.path.join(tmpdir, 'incoming')
+            os.mkdir(incoming)
+            foxsdock.app.config['DIRECTORIES_INCOMING'] = incoming
+            c = foxsdock.app.test_client()
+
+            data = {}
+            pdbf = os.path.join(tmpdir, 'test.pdb')
+            with open(pdbf, 'w') as fh:
+                fh.write("foo")
+
+            saxsf = os.path.join(tmpdir, 'test.profile')
+            with open(saxsf, 'w') as fh:
+                fh.write("0.00000    9656627.00000000 2027.89172363\n")
+
+            data = {'moltype': 'Default', 'jobname': 'foobar',
+                    'weighted': 'on',
+                    'recfile': open(pdbf, 'rb'), 'ligfile': open(pdbf, 'rb'),
+                    'saxsfile': open(saxsf, 'rb')}
+            rv = c.post('/job', data=data)
+            self.assertEqual(rv.status_code, 200)
+
+            with open(os.path.join(incoming, 'foobar', 'input.txt')) as fh:
+                contents = fh.read()
+            self.assertEqual(
+                contents,
+                "test.pdb test.pdb --saxs test.profile "
+                "--complex_type Default --weighted_saxs_score\n")
+
+    def test_submit_empty_profile(self):
+        """Test submit with empty profile"""
+        with tempfile.TemporaryDirectory() as tmpdir:
+            incoming = os.path.join(tmpdir, 'incoming')
+            os.mkdir(incoming)
+            foxsdock.app.config['DIRECTORIES_INCOMING'] = incoming
+            c = foxsdock.app.test_client()
+
+            data = {}
+            pdbf = os.path.join(tmpdir, 'test.pdb')
+            with open(pdbf, 'w') as fh:
+                fh.write("foo")
+
+            saxsf = os.path.join(tmpdir, 'test.profile')
+            with open(saxsf, 'w') as fh:
+                pass
+
+            data = {'moltype': 'Default', 'jobname': 'foobar',
+                    'recfile': open(pdbf, 'rb'), 'ligfile': open(pdbf, 'rb'),
+                    'saxsfile': open(saxsf, 'rb')}
+            rv = c.post('/job', data=data)
+            self.assertEqual(rv.status_code, 400)
+            self.assertIn(b'You have uploaded an empty SAXS profile file',
+                          rv.data)
+
+    def test_submit_empty_pdb(self):
+        """Test submit with empty PDB"""
+        with tempfile.TemporaryDirectory() as tmpdir:
+            incoming = os.path.join(tmpdir, 'incoming')
+            os.mkdir(incoming)
+            foxsdock.app.config['DIRECTORIES_INCOMING'] = incoming
+            c = foxsdock.app.test_client()
+
+            data = {}
+            pdbf = os.path.join(tmpdir, 'test.pdb')
+            with open(pdbf, 'w') as fh:
+                pass
+
+            saxsf = os.path.join(tmpdir, 'test.profile')
+            with open(saxsf, 'w') as fh:
+                fh.write("foo")
+
+            data = {'moltype': 'Default', 'jobname': 'foobar',
+                    'recfile': open(pdbf, 'rb'), 'ligfile': open(pdbf, 'rb'),
+                    'saxsfile': open(saxsf, 'rb')}
+            rv = c.post('/job', data=data)
+            self.assertEqual(rv.status_code, 400)
+            self.assertIn(b'You have uploaded an empty receptor PDB file',
+                          rv.data)
+
+    def test_submit_pdb_code(self):
+        """Test submit with a PDB code"""
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with tempfile.TemporaryDirectory() as pdb_root:
+                foxsdock.app.config['DIRECTORIES_INCOMING'] = tmpdir
+                foxsdock.app.config['PDB_ROOT'] = pdb_root
+                os.mkdir(os.path.join(pdb_root, 'ab'))
+                os.mkdir(os.path.join(pdb_root, 'xy'))
+                with open(os.path.join(pdb_root, 'ab', 'pdb1abc.ent.gz'), 'w'):
+                    pass
+                with open(os.path.join(pdb_root, 'xy', 'pdb2xyz.ent.gz'), 'w'):
+                    pass
+
+                c = foxsdock.app.test_client()
+
+                data = {}
+                saxsf = os.path.join(tmpdir, 'test.profile')
+                with open(saxsf, 'w') as fh:
+                    fh.write("foo")
+
+                data = {'moltype': 'Default', 'jobname': 'foobar',
+                        'receptor': '1abc', 'ligand': '2xyz',
+                        'saxsfile': open(saxsf, 'rb')}
+                rv = c.post('/job', data=data)
+                self.assertEqual(rv.status_code, 200)
+
+    def test_submit_no_pdb(self):
+        """Test submit with no PDB"""
+        with tempfile.TemporaryDirectory() as tmpdir:
+            incoming = os.path.join(tmpdir, 'incoming')
+            os.mkdir(incoming)
+            foxsdock.app.config['DIRECTORIES_INCOMING'] = incoming
+
+            c = foxsdock.app.test_client()
+
+            data = {}
+            saxsf = os.path.join(tmpdir, 'test.profile')
+            with open(saxsf, 'w') as fh:
+                fh.write("foo")
+
+            data = {'moltype': 'Default', 'jobname': 'foobar',
+                    'saxsfile': open(saxsf, 'rb')}
+            rv = c.post('/job', data=data)
+            self.assertEqual(rv.status_code, 400)
+            self.assertIn(b'specify PDB code or upload file for receptor',
+                          rv.data)
+
 
 if __name__ == '__main__':
     unittest.main()
